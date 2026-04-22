be16f6d98f547a6e556b7d33db93c3b08b0027d9
Mateus-R-De-Lima <limamateus67@gmail.com>
2026-04-22 18:37:55 -0300

    feature - Configurando Handler

--- main.go
+++ main.go
@@ -1,4 +1,35 @@
 package main
 
+import (
+	"CRUDUSERS/api"
+	"log/slog"
+	"net/http"
+	"os"
+	"time"
+)
+
 func main() {
+	if err := run(); err != nil {
+
+		slog.Error("failed to run server", "error", err)
+		os.Exit(1)
+	}
+
+	slog.Info("server stopped")
+}
+
+func run() error {
+	handler := api.NewHandler()
+	s := http.Server{
+		ReadTimeout:  10 * time.Second,
+		IdleTimeout:  time.Minute,
+		WriteTimeout: 10 * time.Second,
+		Addr:         ":8080",
+		Handler:      handler,
+	}
+
+	if err := s.ListenAndServe(); err != nil {
+		return err
+	}
+	return nil
 }
